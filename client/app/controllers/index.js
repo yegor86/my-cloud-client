6ff21d8d52aaac6080275b2c3c20bd5bbb2a4165
masdeft <masdeft@gmail.com>
2015-09-14 13:38:10 +0300

    Add a simple angularjs example Adds a simple form with choosing spices in a home page

--- client/app/controllers/index.js
+++ client/app/controllers/index.js
@@ -3,4 +3,11 @@
 angular.module('myCloudDriveApp')
     .controller('IndexCtrl', function($scope) {
         $scope.greeting = 'Hello!';
+
+        $scope.customSpice = "wasabi";
+        $scope.spice = "very";
+
+        $scope.spicy = function(spice) {
+            $scope.spice = spice;
+        }
     });
