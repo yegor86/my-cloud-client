022cf81a4217088664d90f90bc0c6438c562ddac
masdeft <masdeft@gmail.com>
2015-09-15 11:37:23 +0300

    Edit gulpfile.js Adds an inject task to defaults

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 gulp.task('default', function() {
-    console.log('Default task ran');
+    gulp.run('inject');
 });
 
 gulp.task('inject', require('./tasks/inject'));
