b4770f9ddaa1d0cdd03a2a1e3253f720cd4e4b0f
masdeft <masdeft@gmail.com>
2015-09-17 17:24:26 +0300

    Edit gulpfile.js Adds two tasks:     - unit - to run unit tests;     - e2e - to run end to end tests.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    karma = require('karma').server,
+    protractor = require('gulp-protractor').protractor;
+
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -9,3 +12,20 @@ gulp.task('default', function() {
 });
 
 gulp.task('inject', require('./tasks/inject'));
+
+gulp.task('unit', function(done) {
+    karma.start({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done);
+});
+
+gulp.task('e2e', function(done) {
+    var args = ['--baseUrl', 'http://127.0.0.1:9000'];
+    gulp.src(["./client/tests/e2e/*.js"])
+        .pipe(protractor({
+            configFile: "./protractor.conf.js",
+            args: args
+        }))
+        .on('error', function(e) { throw e; });
+});
