76495496ba1164b16cc073086871bc5087fa1ddc
masdeft <masdeft@gmail.com>
2015-09-22 14:48:53 +0300

    Fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var gulp = require('gulp'),
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 gulp.task('default', function() {
-    gulp.run('inject');
+    gulp.start('inject');
 });
 
 gulp.task('inject', require('./tasks/inject'));
