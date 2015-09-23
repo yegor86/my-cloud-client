50f7dc2fc699ffa0959e806e1f2ddebcc93c4c91
masdeft <masdeft@gmail.com>
2015-09-23 11:28:11 +0300

    Inject bower files separately in index.html template

--- tasks/inject.js
+++ tasks/inject.js
@@ -1,16 +1,17 @@
 'use strict';
 
-var gulp = require('gulp');
-var inject = require('gulp-inject');
+var gulp = require('gulp'),
+    inject = require('gulp-inject'),
+    bowerFiles = require('main-bower-files');
 
 module.exports = function() {
     var target = gulp.src('./client/index.html');
     var sources = gulp.src([
         './client/styles/css/*.css',
-        './client/**/*.min.js',
         './client/app/app.js',
         './client/app/controllers/*.js'], {read: false});
 
     return target.pipe(inject(sources, {ignorePath: 'client/'}))
+        .pipe(inject(gulp.src(bowerFiles(), {read: false}), {ignorePath: 'client/', name: 'bower'}))
         .pipe(gulp.dest('./client'));
 };
