d2220be2da42112265318f0217a206e15bc0872a
masdeft <masdeft@gmail.com>
2015-09-25 15:11:14 +0300

    Add a hint task Makes a linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
     karma = require('karma').server,
     protractor = require('gulp-protractor').protractor;
 
@@ -8,7 +9,16 @@ var gulp = require('gulp'),
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 gulp.task('default', function() {
+    gulp.start('jslint');
     gulp.start('inject');
+});
+
+gulp.task('hint', function() {
+    return gulp.src(['./server/app.js',
+        './server/routes/*.js',
+        './server/bin/www'])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default', { verbose: true }));
 });
 
 gulp.task('inject', require('./tasks/inject'));
