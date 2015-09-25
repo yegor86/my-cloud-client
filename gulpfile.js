405ad30293567e60779e3df4f19a58fabe63a5ce
masdeft <masdeft@gmail.com>
2015-09-25 15:41:35 +0300

    Edit gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,8 @@ gulp.task('default', function() {
 gulp.task('hint', function() {
     return gulp.src(['./server/app.js',
         './server/routes/*.js',
-        './server/bin/www'])
+        './server/bin/www',
+        './client/app/**/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default', { verbose: true }));
 });
