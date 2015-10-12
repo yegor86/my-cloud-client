144f31557bc7767d4ec2999ffc453c595cabc563
masdeft <masdeft@gmail.com>
2015-10-12 11:12:25 +0300

    Wrap tasks/inject.js in function

--- tasks/inject.js
+++ tasks/inject.js
@@ -1,17 +1,19 @@
-'use strict';
+(function(module) {
+    "use strict";
 
-var gulp = require('gulp'),
-    inject = require('gulp-inject'),
-    bowerFiles = require('main-bower-files');
+    var gulp = require('gulp'),
+        inject = require('gulp-inject'),
+        bowerFiles = require('main-bower-files');
 
-module.exports = function() {
-    var target = gulp.src('./client/index.html');
-    var sources = gulp.src([
-        './client/assets/css/*.css',
-        './client/app/app.js',
-        './client/app/**/*.js'], {read: false});
+    module.exports = function() {
+        var target = gulp.src('./client/index.html');
+        var sources = gulp.src([
+            './client/assets/css/*.css',
+            './client/app/app.js',
+            './client/app/**/*.js'], {read: false});
 
-    return target.pipe(inject(sources, {ignorePath: 'client/'}))
-        .pipe(inject(gulp.src(bowerFiles(), {read: false}), {ignorePath: 'client/', name: 'bower'}))
-        .pipe(gulp.dest('./client'));
-};
+        return target.pipe(inject(sources, {ignorePath: 'client/'}))
+            .pipe(inject(gulp.src(bowerFiles(), {read: false}), {ignorePath: 'client/', name: 'bower'}))
+            .pipe(gulp.dest('./client'));
+    };
+}(module));
