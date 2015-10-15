ed67da65e8280634fe7d79970699f4d502ca5eb6
masdeft <masdeft@gmail.com>
2015-10-15 16:42:54 +0300

    Refactor IndexCtrl There is no callback function now. A function constructor is passed to module.controller()

--- client/app/controllers/IndexCtrl.js
+++ client/app/controllers/IndexCtrl.js
@@ -1,8 +1,11 @@
 (function(angular) {
     "use strict";
 
-    angular.module('myCloudDriveApp')
-        .controller('IndexCtrl', function($scope, FileNavigator) {
-            $scope.items = FileNavigator.query();
-        });
+    var module = angular.module('myCloudDriveApp');
+
+    function IndexCtrl($scope, FileNavigator) {
+        $scope.items = FileNavigator.query();
+    }
+
+    module.controller('IndexCtrl', IndexCtrl);
 }(angular));
