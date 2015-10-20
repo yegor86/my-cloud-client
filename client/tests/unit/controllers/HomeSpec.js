d879116ef08983e6a35501da5fa2276555cffb5b
masdeft <masdeft@gmail.com>
2015-10-20 12:28:26 +0300

    Fix HomeSpec.js unit test Sets mocks of GET requests: GET partials/home.html, GET partials/home.files.html Now this works if httpBackend.flush() is called

--- client/tests/unit/controllers/HomeSpec.js
+++ client/tests/unit/controllers/HomeSpec.js
@@ -7,17 +7,22 @@ describe('HomeCtrl', function() {
     beforeEach(inject(function($httpBackend, $rootScope, $controller, $state) {
         httpBackend = $httpBackend;
         state = $state;
+
         httpBackend.expectGET('/files//files.json')
             .respond([
                 {name: "some-image.jpg", type: "image"},
                 {name: "some-text.txt", type: "text"},
                 {name: "some-dir", type: "dir"}
             ]);
+        httpBackend.expectGET('partials/home.html').respond('<html><head></head><body></body></html>');
+        httpBackend.expectGET('partials/home.files.html').respond('<ol><li></li></ol>');
 
         scope = $rootScope.$new();
         ctrl = $controller('HomeCtrl', {$scope: scope});
     }));
 
     it('should be 3 items', function() {
+        httpBackend.flush();
+        expect(scope.items.length).toBe(3);
     });
 });
