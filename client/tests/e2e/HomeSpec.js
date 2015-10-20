099381ab7fbf51f281446683d50e755f8d31cbed
masdeft <masdeft@gmail.com>
2015-10-20 13:49:31 +0300

    Edit HomeSpec.js e2e test Adds a new case "should open a dir and then return back"

--- client/tests/e2e/HomeSpec.js
+++ client/tests/e2e/HomeSpec.js
@@ -7,11 +7,24 @@ describe('myCloudDriveApp', function() {
     });
 
     it('should be a home link', function() {
-        var linkAbout = element(by.css('a'));
-        linkAbout.click();
+        var link = element(by.css('a'));
+        link.click();
 
-        var linkHome = element(by.css('a'));
+        expect(link.getText()).toBe('home');
+    });
 
-        expect(linkHome.getText()).toBe('home');
+    it('should open a dir and then return back', function() {
+        var dirCssClass = '.sprite-dir',
+            dirElement = element(by.css(dirCssClass));
+
+        if (dirElement.isPresent() === true) {
+            var dirs = element.all(by.css(dirCssClass)),
+                dirsCount = dirs.length;
+
+            dirs.get(0).click();
+            browser.navigate().back();
+            dirs = element.all(by.css(dirCssClass));
+            expect(dirs.length).toBe(dirsCount);
+        }
     });
 });
