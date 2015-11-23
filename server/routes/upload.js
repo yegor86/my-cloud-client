d641a76d3522ca35e62a6554d68199ef2bf6d350
masdeft <masdeft@gmail.com>
2015-11-23 12:36:53 +0200

    Use "http-proxy" module to proxy upload requests to the web server

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -2,31 +2,13 @@
     "use strict";
 
     var http = require('http'),
-        querystring = require('querystring'),
-        FormData = require('form-data');
+        httpProxy = require('http-proxy');
 
     module.exports = function(router) {
-        router.post('/files%2Fupload', function(clientRequest, clientResponse) {
-            var options = {
-                    host: '127.0.0.1',
-                    path: '/files/upload',
-                    port: '3030',
-                    method: 'POST',
-                    header: {'Content-Type': 'application/x-www-form-urlencoded'}};
+        router.post('/files/upload', function(clientRequest, clientResponse) {
+            var proxy = httpProxy.createProxyServer();
 
-            var serverRequest = http.request(options, function(serverResponse) {
-                console.log(serverResponse.statusMessage);
-            });
-
-            var formData = new FormData();
-            formData.append('file', clientRequest);
-            serverRequest.write(querystring.stringify(formData));
-
-            serverRequest.on('error', function(error) {
-                clientResponse.statusCode = 500;
-                clientResponse.json([{hasError: true, errorMessage: error.message, errorCode: error.code}]);
-            });
-            serverRequest.end();
+            proxy.web(clientRequest, clientResponse, {target: 'http://127.0.0.1:3030'});
         });
         return router;
     };
