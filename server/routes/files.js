93b41cf3ef66e104551ca2ecbc77b6f767c4e547
max.fadeev <masdeft@gmail.com>
2015-11-26 21:32:00 +0200

    Add a server config

--- server/routes/files.js
+++ server/routes/files.js
@@ -1,38 +1,29 @@
-(function(module) {
+(function (module) {
     "use strict";
 
-    var http = require('http');
+    var http = require('http'),
+        server = require('../config/server');
 
-    module.exports = function(router) {
-        router.get('/files(\/?[a-zA-Z0-9_-]?)*', function(clientRequest, clientResponse) {
+    module.exports = function (router) {
+
+        function cleanUrl(url) {
+            // Remove encoded slashes
+            return url.replace(/%2F/g, '/');
+        }
+
+        router.get('/files(\/?[a-zA-Z0-9_-]?)*', function (clientRequest, clientResponse) {
             var options = {
-                host: '127.0.0.1',
-                path: clientRequest.url.replace(/%2F/g, '/').replace('/files', '/files/list/admin@mail.com'),
-                port: '3030',
-                method: 'GET'};
+                    host: server.host,
+                    path: cleanUrl(clientRequest.url).replace('/files', '/files/list/admin@mail.com'),
+                    port: server.port,
+                    method: 'GET'};
 
-            var serverRequest = http.request(options, function(serverResponse) {
-                serverResponse.on('data', function(chunk) {
-                    var files = [],
-                        data;
-
-                    try {
-                        data = JSON.parse(chunk);
-                        data.forEach(function(item) {
-                            files.push({
-                                name: item.fileName || '',
-                                type: item.extension || (item.folder === true ? 'dir' : ''),
-                                size: item.fileSize || '',
-                                modified: '',
-                                sharedWith: []});
-                        });
-                    } catch(error) {
-                        return console.error(error.message);
-                    }
-                    clientResponse.send(files);
+            var serverRequest = http.request(options, function (serverResponse) {
+                serverResponse.on('data', function (chunk) {
+                    clientResponse.send(chunk);
                 });
             });
-            serverRequest.on('error', function(error) {
+            serverRequest.on('error', function (error) {
                 clientResponse.statusCode = 500;
                 clientResponse.json([{hasError: true, errorMessage: error.message, errorCode: error.code}]);
             });
