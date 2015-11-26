6ac4d2a2be1da261fefdf1a65af2b7e13817178c
masdeft <masdeft@gmail.com>
2015-11-26 18:01:35 +0200

    Fix a file uploading

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,11 +1,10 @@
-(function(module) {
+(function (module) {
     "use strict";
 
-    var http = require('http'),
-        httpProxy = require('http-proxy');
+    var httpProxy = require('http-proxy');
 
-    module.exports = function(router) {
-        router.post('/files/upload', function(clientRequest, clientResponse) {
+    module.exports = function (router) {
+        router.post('/files/upload', function (clientRequest, clientResponse) {
             var proxy = httpProxy.createProxyServer();
 
             proxy.web(clientRequest, clientResponse, {target: 'http://127.0.0.1:3030'});
