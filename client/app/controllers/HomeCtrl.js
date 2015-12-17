413bdfc7b1115ad5386966468e7769898677ce67
masdeft <masdeft@gmail.com>
2015-12-17 14:35:24 +0200

    Move breadcrumbs to a respective directive

--- client/app/controllers/HomeCtrl.js
+++ client/app/controllers/HomeCtrl.js
@@ -4,32 +4,6 @@
     var module = angular.module('myCloudDriveApp');
 
     function HomeCtrl($scope) {
-
-        $scope.setBreadcrumbs = function (url) {
-            var folders = [],
-                paths,
-                folderPath = '';
-
-            if (url[url.length - 1] === '/') {
-                url = url.slice(0, -1);
-            }
-
-            paths = url.split('/');
-
-            for (var i = 0; i < paths.length; i++) {
-                folderPath = [folderPath, paths[i]].join("/");
-                folders.push({
-                    name: paths[i],
-                    path: folderPath
-                });
-            }
-
-            folders[0].name = 'MyCloudDrive';
-
-            $scope.currentFolder = folders.pop();
-            $scope.folders = folders;
-        };
-
         // Set a default sort type
         $scope.sortType = 'name';
     }
