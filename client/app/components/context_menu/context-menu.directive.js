2f0cba80e4d701fdaf84350437dd606d5729cf56
masdeft <masdeft@gmail.com>
2016-04-22 12:02:11 +0300

    Move the DOM related logic from the context menu service to the directive

--- client/app/components/context_menu/context-menu.directive.js
+++ client/app/components/context_menu/context-menu.directive.js
@@ -6,7 +6,7 @@
         .directive('mccContextMenu', contextMenu);
 
     /* @ngInject */
-    function contextMenu($document, contextMenuService) {
+    function contextMenu($document) {
         return {
             link: link,
             templateUrl: 'app/components/context_menu/context-menu.html',
@@ -15,20 +15,32 @@
             restrict: 'A'
         };
 
-        function link(scope, element) {
+        function link(scope, element, attrs, contextMenu) {
             // Trigger right click on the document
             $document.bind('contextmenu', function (event) {
                 // Prevent a default context menu
                 event.preventDefault();
 
-                contextMenuService.createMenu('document', event);
+                contextMenu.create(element, 'document', event);
             });
 
             $document.bind('click', function (event) {
-                contextMenuService.hideMenu();
+                contextMenu.hide(element);
             });
 
-            contextMenuService.element = element;
+            scope.$on('rightClickOnFile', function (event, mouseEvent, item) {
+                //noinspection JSUnresolvedFunction
+                event.stopPropagation();
+                contextMenu.create(element, item.type, mouseEvent);
+                scope.fileName = item.name;
+            });
+
+            scope.$on('rightClickOnAction', function (event, action) {
+                //noinspection JSUnresolvedFunction
+                event.stopPropagation();
+                contextMenu.click(action);
+                contextMenu.hide(element);
+            });
         }
     }
 
@@ -36,13 +48,11 @@
     function ContextMenuController($scope, downloadService, contextMenuService) {
         var vm = this;
         vm.click = clickOnAction;
-        vm.service = contextMenuService;
+        vm.create = createMenu;
+        vm.hide = hideMenu;
+        vm.isShown = false;
 
-        function clickOnAction(action, event) {
-            // Hides the menu in case the right click on the action
-            if (event.type === 'contextmenu') {
-                contextMenuService.hideMenu();
-            }
+        function clickOnAction(action) {
             switch (action.name) {
                 case "upload":
                     $scope.$emit('upload');
@@ -56,6 +66,35 @@
                 case "share-folder":
                     $scope.$emit('shareFolder');
                     break;
+            }
+        }
+        
+        function createMenu(element, menuType, event) {
+            setPosition(element, event);
+            if (vm.menuType !== menuType) {
+                vm.actions = contextMenuService.getActions(menuType);
+                vm.menuType = menuType;
+                $scope.$apply();
+            }
+            showMenu(element);
+        }
+
+        function setPosition(element, event) {
+            element.css('top', contextMenuService.getTopPosition(element, event) + 'px');
+            element.css('left', contextMenuService.getLeftPosition(element, event) + 'px');
+        }
+        
+        function showMenu(element) {
+            if (vm.isShown === false && vm.menuType !== undefined) {
+                vm.isShown = true;
+                element.addClass('opened');
+            }       
+        }
+        
+        function hideMenu(element) {
+            if (vm.isShown === true) {
+                vm.isShown = false;
+                element.removeClass('opened');
             }
         }
     }
--- client/app/components/context_menu/context-menu.service.js
+++ client/app/components/context_menu/context-menu.service.js
@@ -3,16 +3,10 @@
 
     angular
         .module('app.context-menu')
-        .service('contextMenuService', contextMenuService);
+        .factory('contextMenuService', contextMenuService);
 
     /* @ngInject */
     function contextMenuService($document) {
-        var menu = this;
-        menu.getActions = getActions;
-        menu.createMenu = createMenu;
-        menu.hideMenu = hideMenu;
-        menu.isShown = false;
-
         var MARGIN_BOTTOM = 10;
         var actions = {
             download: {name: 'download', title: 'Download'},
@@ -24,6 +18,12 @@
             newFolder: {name: 'create-folder', title: 'New folder'},
             share: {name: 'share-folder', title: 'Share...'}};
 
+        return {
+            getActions: getActions,
+            getTopPosition: getTopPosition,
+            getLeftPosition: getLeftPosition
+        };
+
         function getActions(type) {
             if (type === 'document') {
                 return [actions.upload, actions.newFolder];
@@ -34,44 +34,10 @@
             }
         }
 
-        function createMenu(menuType, event) {
-            setPosition(event);
-            if (menu.type !== menuType) {
-                menu.scope = menu.element.scope();
-                menu.scope.$apply(function () {
-                    menu.actions = getActions(menuType);
-                });
-                menu.type = menuType;
-            }
-            showMenu();
-        }
-
-        function showMenu() {
-            if (menu.isShown === false && menu.type !== undefined) {
-                menu.isShown = true;
-                menu.element.addClass('opened');
-            }
-        }
-
-        function hideMenu() {
-            if (menu.isShown === true) {
-                menu.isShown = false;
-                menu.element.removeClass('opened');
-            }
-        }
-
-        function setPosition(event) {
-            var top = getTopPosition(event);
-            var left = getLeftPosition(event);
-
-            menu.element.css('top', top + 'px');
-            menu.element.css('left', left + 'px');
-        }
-
-        function getTopPosition(event) {
+        function getTopPosition(element, event) {
             var doc = $document[0].documentElement,
                 docTop = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0),
-                elementHeight = menu.element[0].scrollHeight,
+                elementHeight = element[0].scrollHeight,
                 docHeight = doc.clientHeight + docTop,
                 totalHeight = elementHeight + event.pageY,
                 top = Math.max(event.pageY - docTop, 0);
@@ -83,10 +49,10 @@
             return top;
         }
 
-        function getLeftPosition(event) {
+        function getLeftPosition(element, event) {
             var doc = $document[0].documentElement,
                 docLeft = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0),
-                elementWidth = menu.element[0].scrollWidth,
+                elementWidth = element[0].scrollWidth,
                 docWidth = doc.clientWidth + docLeft,
                 totalWidth = elementWidth + event.pageX,
                 left = Math.max(event.pageX - docLeft, 0);
