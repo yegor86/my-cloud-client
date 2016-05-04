ca6dddfb81fc1f3d4bf5506d98a118255d9000e6
masdeft <masdeft@gmail.com>
2016-05-04 11:42:54 +0300

    Fix the download route

--- server/routes/download.js
+++ server/routes/download.js
@@ -1,38 +1,13 @@
 (function (module) {
     "use strict";
 
-    var http = require('http'),
-        server = require('../config/server');
+    var httpProxy = require('http-proxy');
 
     module.exports = function (router) {
-
-        function cleanUrl(url) {
-            // Remove encoded slashes
-            return url.replace(/%2F/g, '/');
-        }
-
         router.get('/files/download(\/?[a-zA-Z0-9_-]?)*', function (clientRequest, clientResponse) {
-            
-            var options = {
-                    host: server.host,
-                    path: cleanUrl(clientRequest.url),
-                    port: server.port,
-                    method: 'GET'};
+            var proxy = httpProxy.createProxyServer();
 
-            var serverRequest = http.request(options, function (serverResponse) {
-                var body = [];
-                serverResponse.on('data', function (chunk) {
-                    body.push(chunk);
-                }).on('end', function () {
-                    body = Buffer.concat(body).toString();
-                    clientResponse.send(body);
-                });
-            });
-            serverRequest.on('error', function (error) {
-                clientResponse.statusCode = 500;
-                clientResponse.json([{hasError: true, errorMessage: error.message, errorCode: error.code}]);
-            });
-            serverRequest.end();
+            proxy.web(clientRequest, clientResponse, {target: 'http://127.0.0.1:3030'});
         });
         return router;
     };
